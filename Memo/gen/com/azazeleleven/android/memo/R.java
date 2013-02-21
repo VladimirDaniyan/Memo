5b77b339e42b231a1bec70fd03063acddf776e48
Vladimir Daniyan <azazel11@gmail.com>
2013-02-21 12:09:35 -0500

    Add greenDAO library

--- Memo/gen/com/azazeleleven/android/memo/R.java
+++ Memo/gen/com/azazeleleven/android/memo/R.java
@@ -17,31 +17,32 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080003;
-        public static final int button1=0x7f080001;
-        public static final int editText1=0x7f080002;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090003;
+        public static final int editText1=0x7f090001;
+        public static final int listView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int add_note_fragment=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int show_dialog=0x7f050004;
-        public static final int title_activity_main=0x7f050003;
+        public static final int action_settings=0x7f060003;
+        public static final int add_new_note=0x7f060006;
+        public static final int app_name=0x7f060000;
+        public static final int extension_description=0x7f060002;
+        public static final int extension_title=0x7f060001;
+        public static final int show_dialog=0x7f060005;
+        public static final int title_activity_main=0x7f060004;
     }
     public static final class style {
         /** 
@@ -65,10 +66,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
--- Memo/src/com/azazeleleven/android/memo/MainActivity.java
+++ Memo/src/com/azazeleleven/android/memo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.azazeleleven.android.memo;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.azazeleleven.android.memo.DaoMaster.DevOpenHelper;
 
 public class MainActivity extends FragmentActivity {
+
+	private SQLiteDatabase db;
+
+	private EditText editText;
+
+	private DaoMaster daoMaster;
+	private DaoSession daoSession;
+	private NoteDao memoDao;
+
+	private Cursor cursor;
+
+	public static final CharSequence PREF_NAME = "pref_name";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,27 @@ public class MainActivity extends FragmentActivity {
 
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND,
 				WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+		
+		editText = (EditText) findViewById(R.id.editText1);
+		ListView lv = (ListView) findViewById(R.id.listView1);
+
+		DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "memo-db",
+				null);
+		db = helper.getWritableDatabase();
+		daoMaster = new DaoMaster(db);
+		daoSession = daoMaster.newSession();
+		memoDao = daoSession.getNoteDao();
+
+		String textColumn = NoteDao.Properties.Text.columnName;
+		cursor = db.query(memoDao.getTablename(), memoDao.getAllColumns(),
+				null, null, null, null, null, null);
+		String[] from = { textColumn };
+		int[] to = { android.R.id.text1, };
+
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				android.R.layout.simple_expandable_list_item_1, cursor, from,
+				to);
+		lv.setAdapter(adapter);
 
 	}
 
@@ -26,5 +63,7 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
 
 }
