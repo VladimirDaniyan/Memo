0cd2fbb15c73f1586785442caeb571cfe953441b
Vladimir Daniyan <azazel11@gmail.com>
2013-02-24 02:58:07 -0500

    Implemented separate activity for editing memos

--- Memo/gen/com/azazeleleven/android/memo/R.java
+++ Memo/gen/com/azazeleleven/android/memo/R.java
@@ -24,25 +24,35 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090000;
-        public static final int action_settings=0x7f090003;
-        public static final int button1=0x7f090002;
-        public static final int editText1=0x7f090001;
+        public static final int LinearLayout1=0x7f0a0001;
+        public static final int action_add=0x7f0a0004;
+        public static final int action_settings=0x7f0a0003;
+        public static final int editText1=0x7f0a0000;
+        public static final int listView1=0x7f0a0002;
+    }
+    public static final class integer {
+        public static final int lines=0x7f060000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_edit_memo=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_memo_list=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int edit_memo=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int memo_list=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f060003;
-        public static final int add_new_note=0x7f060006;
-        public static final int app_name=0x7f060000;
-        public static final int extension_description=0x7f060002;
-        public static final int extension_title=0x7f060001;
-        public static final int show_dialog=0x7f060005;
-        public static final int title_activity_main=0x7f060004;
+        public static final int action_settings=0x7f070003;
+        public static final int add_new_note=0x7f070006;
+        public static final int app_name=0x7f070000;
+        public static final int extension_description=0x7f070002;
+        public static final int extension_title=0x7f070001;
+        public static final int hello_world=0x7f070008;
+        public static final int show_dialog=0x7f070005;
+        public static final int title_activity_edit_memo=0x7f070007;
+        public static final int title_activity_main=0x7f070004;
     }
     public static final class style {
         /** 
@@ -66,11 +76,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
--- Memo/src/com/azazeleleven/android/memo/MainActivity.java
+++ Memo/src/com/azazeleleven/android/memo/MainActivity.java
@@ -1,27 +1,21 @@
 package com.azazeleleven.android.memo;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
-
-import com.azazeleleven.android.memo.DaoMaster.DevOpenHelper;
 
 public class MainActivity extends ListActivity {
 
 	private SQLiteDatabase db;
 
-	private EditText editText;
-
-	private DaoMaster daoMaster;
-	private DaoSession daoSession;
 	private NoteDao memoDao;
 
 	private Cursor cursor;
@@ -36,44 +30,22 @@ public class MainActivity extends ListActivity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND,
 				WindowManager.LayoutParams.FLAG_DIM_BEHIND);
 
-		editText = (EditText) findViewById(R.id.editText1);
-
-		DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "memo-db",
-				null);
-		db = helper.getWritableDatabase();
-		daoMaster = new DaoMaster(db);
-		daoSession = daoMaster.newSession();
-		memoDao = daoSession.getNoteDao();
-
-		String textColumn = NoteDao.Properties.Text.columnName;
-		cursor = db.query(memoDao.getTablename(), memoDao.getAllColumns(),
-				null, null, null, null, null, null);
-		String[] from = { textColumn };
-		int[] to = { android.R.id.text1, };
-
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_expandable_list_item_1, cursor, from,
-				to);
-		setListAdapter(adapter);
-
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
-	public void addNote(View view) {
-		String memoText = editText.getText().toString();
-		editText.setText("");
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
 
-		Note memo = new Note(null, memoText, null, null);
-		memoDao.insert(memo);
-		Log.d("Memo", "Inserted new memo, ID: " + memo.getId());
-
-		cursor.requery();
+	public void addNote(MenuItem item) {
+		Intent intent = new Intent(this, EditMemoActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
@@ -81,6 +53,5 @@ public class MainActivity extends ListActivity {
 		memoDao.deleteByKey(id);
 		cursor.requery();
 	}
-
 
 }
