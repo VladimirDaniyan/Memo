43b9034373af4fa9aaee77579d0676616b465a61
Vladimir Daniyan <azazel11@gmail.com>
2013-02-24 23:00:32 -0500

    Implemented preliminary status notification and context menu for deleting items in list

--- Memo/gen/com/azazeleleven/android/memo/R.java
+++ Memo/gen/com/azazeleleven/android/memo/R.java
@@ -22,6 +22,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_stat_name=0x7f020001;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f0a0004;
@@ -52,9 +53,9 @@ public final class R {
         public static final int app_name=0x7f070000;
         public static final int extension_description=0x7f070002;
         public static final int extension_title=0x7f070001;
-        public static final int hello_world=0x7f070008;
+        public static final int menu_delete=0x7f070007;
         public static final int show_dialog=0x7f070005;
-        public static final int title_activity_edit_memo=0x7f070007;
+        public static final int title_activity_edit_memo=0x7f070008;
         public static final int title_activity_main=0x7f070004;
     }
     public static final class style {
--- Memo/src/com/azazeleleven/android/memo/MainActivity.java
+++ Memo/src/com/azazeleleven/android/memo/MainActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.SimpleCursorAdapter;
 
 import com.azazeleleven.android.memo.DaoMaster.DevOpenHelper;
@@ -14,6 +18,8 @@ import com.azazeleleven.android.memo.DaoMaster.DevOpenHelper;
 public class MainActivity extends ListActivity {
 
 	public static final CharSequence PREF_NAME = "pref_name";
+
+	private static final int DELETE_ID = Menu.FIRST;
 
 	private DaoMaster daoMaster;
 	private DaoSession daoSession;
@@ -45,6 +51,8 @@ public class MainActivity extends ListActivity {
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
 				android.R.layout.simple_list_item_2, cursor, from, to);
 		setListAdapter(adapter);
+		
+		registerForContextMenu(getListView());
 	}
 
 	@SuppressWarnings("deprecation")
@@ -52,6 +60,24 @@ public class MainActivity extends ListActivity {
 	protected void onResume() {
 		cursor.requery();
 		super.onResume();
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case DELETE_ID:
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+			memoDao.deleteByKey(info.id);
+			cursor.requery();
+		}
+		return super.onContextItemSelected(item);
 	}
 
 	@Override
