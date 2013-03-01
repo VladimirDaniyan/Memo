cb5e7846b3005837a2a2709362447b43ca8f65a4
Vladimir Daniyan <azazel11@gmail.com>
2013-02-28 23:31:15 -0500

    Added DashClock Extension support

--- Memo/gen/com/azazeleleven/android/memo/R.java
+++ Memo/gen/com/azazeleleven/android/memo/R.java
@@ -17,47 +17,45 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_stat_name=0x7f020001;
+        public static final int ic_extension=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_stat_name=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f0a0005;
-        public static final int LinearLayout2=0x7f0a0000;
-        public static final int action_add=0x7f0a0007;
-        public static final int action_settings=0x7f0a0006;
-        public static final int button_cancel=0x7f0a0003;
-        public static final int button_ok=0x7f0a0004;
-        public static final int checkbox_add_to_notification=0x7f0a0002;
-        public static final int edit_text_memo=0x7f0a0001;
+        public static final int LinearLayout1=0x7f090005;
+        public static final int LinearLayout2=0x7f090000;
+        public static final int action_add=0x7f090006;
+        public static final int button_cancel=0x7f090003;
+        public static final int button_ok=0x7f090004;
+        public static final int checkbox_add_to_notification=0x7f090002;
+        public static final int edit_text_memo=0x7f090001;
     }
     public static final class integer {
-        public static final int lines=0x7f060000;
+        public static final int lines=0x7f050000;
     }
     public static final class layout {
         public static final int activity_edit_memo=0x7f030000;
         public static final int activity_list_memo=0x7f030001;
     }
     public static final class menu {
-        public static final int edit_memo=0x7f090000;
-        public static final int main=0x7f090001;
-        public static final int memo_list=0x7f090002;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070003;
-        public static final int add_new_note=0x7f070006;
-        public static final int app_name=0x7f070000;
-        public static final int create_notification=0x7f070009;
-        public static final int extension_description=0x7f070002;
-        public static final int extension_title=0x7f070001;
-        public static final int menu_delete=0x7f070007;
-        public static final int show_dialog=0x7f070005;
-        public static final int title_activity_edit_memo=0x7f070008;
-        public static final int title_activity_main=0x7f070004;
-        public static final int toast_input_required=0x7f07000a;
+        public static final int action_settings=0x7f060003;
+        public static final int add_new_note=0x7f060006;
+        public static final int app_name=0x7f060000;
+        public static final int create_notification=0x7f060009;
+        public static final int extension_description=0x7f060002;
+        public static final int extension_title=0x7f060001;
+        public static final int menu_delete=0x7f060007;
+        public static final int show_dialog=0x7f060005;
+        public static final int title_activity_edit_memo=0x7f060008;
+        public static final int title_activity_main=0x7f060004;
+        public static final int toast_input_required=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -81,13 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
-    }
-    public static final class xml {
-        public static final int preferences=0x7f040000;
+        public static final int AppTheme=0x7f070001;
     }
 }
