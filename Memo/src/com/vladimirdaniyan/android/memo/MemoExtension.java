6aa06d66b37dedea73b3f7b9ad5c445382631991
Vladimir Daniyan <azazel11@gmail.com>
2013-03-10 20:04:53 -0400

    show most recent memo in expanded dashclock

--- Memo/src/com/vladimirdaniyan/android/memo/MemoExtension.java
+++ Memo/src/com/vladimirdaniyan/android/memo/MemoExtension.java
@@ -1,19 +1,56 @@
 package com.vladimirdaniyan.android.memo;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
+import com.azazeleleven.android.memo.DaoMaster;
+import com.azazeleleven.android.memo.DaoMaster.DevOpenHelper;
+import com.azazeleleven.android.memo.DaoSession;
+import com.azazeleleven.android.memo.Note;
+import com.azazeleleven.android.memo.NoteDao;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
-import com.vladimirdaniyan.android.memo.R;
 
 public class MemoExtension extends DashClockExtension {
+
+	private DaoMaster daoMaster;
+	private DaoSession daoSession;
+	private NoteDao memoDao;
+	private Note memo;
+
+	private SQLiteDatabase db;
+	private Cursor cursor;
+
+	@Override
+	protected void onInitialize(boolean isReconnect) {
+		super.onInitialize(isReconnect);
+		setUpdateWhenScreenOn(true);
+	}
 
 	@Override
 	protected void onUpdateData(int reason) {
 		final Intent addMemoIntent = getAddMemoIntent();
 
+		DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "memo-db",
+				null);
+		db = helper.getReadableDatabase();
+		daoMaster = new DaoMaster(db);
+		daoSession = daoMaster.newSession();
+		memoDao = daoSession.getNoteDao();
+
+		cursor = db.query(memoDao.getTablename(), memoDao.getAllColumns(),
+				null, null, null, null, null);
+		cursor.moveToLast();
+		
+		memo = memoDao.readEntity(cursor, 0);
+
+		String memoText = null;
+		memoText = memo.getText();
+
 		publishUpdate(new ExtensionData().visible(true)
 				.icon(R.drawable.ic_stat_memo).status("Add Memo")
+				.expandedTitle("Most Recent Memo").expandedBody(memoText)
 				.clickIntent(addMemoIntent));
 
 	}
